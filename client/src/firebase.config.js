265ccff8289c1acee5acc956c6fcab363869522d
Meet Soni <meetsoni3017@gmail.com>
2022-04-24 23:01:54 +0530

    add config vars

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase/app";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
