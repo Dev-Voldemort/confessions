eb44dbd9974b7f57284f3806d391b1c8af91f30f
Meet Soni <meetsoni3017@gmail.com>
2022-04-25 08:59:50 +0530

    resolve gpa icon

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(props){
   return (
     <header>
-    <img className="gp-img" src="http://www.gpah.cteguj.in/uploads/8338/Icon.png"/>
+    <img className="gp-img" src="../public/gpa-icon.png"/>
       <h1>GPA Confession</h1>
       <a onClick={props.onClick} className="btn" >{props.name}  {props.name === "Login" ? <i className="fa fa-sign-in" aria-hidden="true"></i> : <i className="fa fa-sign-out" aria-hidden="true"></i>}</a>
     </header>
