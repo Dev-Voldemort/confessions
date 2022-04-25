fe4b2076426c38a95c11df2f405c8dc5b778ca64
Meet Soni <meetsoni3017@gmail.com>
2022-04-25 09:17:40 +0530

    resolve

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header(props){
   return (
     <header>
-    <img className="gp-img" src="../public/gpa-icon.png"/>
+    <img className="gp-img" src="../gpa-icon.png"/>
       <h1>GPA Confession</h1>
       <a onClick={props.onClick} className="btn" >{props.name}  {props.name === "Login" ? <i className="fa fa-sign-in" aria-hidden="true"></i> : <i className="fa fa-sign-out" aria-hidden="true"></i>}</a>
     </header>
   );  
-  }
+}
--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,8 +1,7 @@
 import * as firebase from "firebase/app";
 
 const app = firebase.initializeApp({
-  // apiKey: process.env.REACT_APP_API_KEY,
-  apiKey: "AIzaSyD4Y4TzCiNoHukeWJo22e91VDyvzOy7YF0",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
